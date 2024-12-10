500ef67e789698ee8adba76b6336264d213f3109
Rick Rutten <rick.rutten@vodafoneziggo.com>
2024-12-10 11:35:14 +0100

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,24 +1,29 @@
-prijzen = {
-    "aardbei" : "3",
-    "vanille" : "4",
-    "chocolade" : "5"
-}
+from helper import decoreer
 
-aanbieding = int(prijzen["aardbei"])* 0.8
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
 
-reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
+    aanbieding = prijzen["aardbei"]* 0.8
 
-einde = int((reclame_tekst.index("0")))
-reclame_tekst2 = reclame_tekst[:62]
+    reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
 
-reclame_tekst3 = reclame_tekst2.upper()
+    reclame_tekst2 = reclame_tekst[:63]
 
-reclame_tekst4 = reclame_tekst3.split(" ")
+    reclame_tekst3 = reclame_tekst2.upper()
 
-for el in reclame_tekst4:
-    if len(el) >= 5:
-        print(el.upper())
-    else:
-        print(el.lower())
-    
+    reclame_tekst4 = reclame_tekst3.split()
+
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
+        
     
